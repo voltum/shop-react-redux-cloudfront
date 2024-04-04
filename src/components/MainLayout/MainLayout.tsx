c04b9f9080c665e5b2620175d97ec0852a22f1f6
Sarvar Mutalov <mutalov02@mail.ru>
2024-04-05 01:45:12 +0500

    feat: automated deployment with serverless plugin implemented

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,7 +8,7 @@ import Box from "@mui/material/Box";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
+      {"Copyright © Voltum"}
       <Link color="inherit" href="https://material-ui.com/" underline="hover">
         My Store
       </Link>{" "}
